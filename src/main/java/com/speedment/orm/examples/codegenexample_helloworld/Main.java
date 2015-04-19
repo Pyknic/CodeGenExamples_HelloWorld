a321491cbcf0c65e0ff6a8d6c525c2c36f0e1ef8
Pyknic <emil.forslund@pyknic.se>
2015-04-19 19:04:48 +0200

    Change param type to triple dots

--- src/main/java/com/speedment/orm/examples/codegenexample_helloworld/Main.java
+++ src/main/java/com/speedment/orm/examples/codegenexample_helloworld/Main.java
@@ -24,7 +24,7 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String... args) {
         OUTPUT.write(new JavaGenerator().on(
             File.of(FILENAME)
                 .add(Class.of("BasicExample")
